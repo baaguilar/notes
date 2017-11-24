ffdaddeee8e438235b2804bf73e02117836100ec
Your Name <baguilar@marketwatch.com>
2017-11-24 16:07:35 -0500

    minor cleanup

--- js/script.js
+++ js/script.js
@@ -8,8 +8,11 @@ $(document).ready(function(){
     // initialize a new array for use later
     var txt = [];
     
+    var title = [];
+    
     // add a first item to the array
     txt[1] = "My notes for Nov. 24." ;   
+    title[1] = "11/24 3:43 PM";
     
     //when the user clicks, the modal is filled with the correct content
    
@@ -17,27 +20,40 @@ $(document).ready(function(){
     $("#yournotes").on("click",".notes",function(){
         theid = this.id;
         fillNote();
+        setTimeout(function() {
+          $('#comment').focus();
+        }, 500);
     });
     
     $("#new").on("click",function(){
         addNew();
     });
-    
+        
     // script to add new button
     function addNew(){
         i++; // increment +1
-        theid = i;
+        theid = i; // set theid to match "i"
+        title[i] = timeStamp(); // get timestamp and add that value to our array
+        
+        // add a new button
         $("<div>",{class:"notes",id:i}).appendTo("#yournotes");
         $("<button>",{type:"button",class:"btn btn-primary",id:"note"+i}).appendTo("#"+i);
         $("#comment").val("");
-        $("#note"+i).attr( "data-toggle", "modal" ).attr( "data-target", "#note" ).text("Note - "+timeStamp());
-        console.log(i);
+        $("#note"+i).attr( "data-toggle", "modal" ).attr( "data-target", "#note" ).text("Note - "+title[i]);
+        
+        //update .modal-title with timestamp
+        $(".modal-title").text("Notes - "+title[i]);
+        
+        setTimeout(function() {
+          $('#comment').focus();
+        }, 500);
     }
     
     // function that will handle placing the correct text inside the modal textbox
     function fillNote(){
         
         $("#comment").val(txt[theid]);
+        $(".modal-title").text("Notes - "+title[theid]);
         
         for(var index in txt){
             console.log(index+" "+txt[index]);
@@ -52,7 +68,7 @@ $(document).ready(function(){
     
     // function to handle the close+save process
     function closeModal(){
-        console.log("theid: "+theid+", i:"+i);
+        console.log("theid: "+theid+", i:"+i+" - "+title[theid]);
         txt[theid] = $("#comment").val();
         
     }
