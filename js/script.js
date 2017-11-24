e8f3876d2e54db4b43d5be08eedcef70e39c8aff
Your Name <baguilar@marketwatch.com>
2017-11-24 15:46:06 -0500

    modifications. can now add as many buttons as you'd like

--- js/script.js
+++ js/script.js
@@ -1,42 +1,48 @@
 $(document).ready(function(){
+    // initialize a variable "i" for use later
+    var i = 1;
+    
+    // initialize a variable "theid" for use later
+    var theid;
 
-    // text for Nov. 12 and Dec. 5
-    var novTxt = "My notes for Nov. 12."
-    var decTxt = "";
+    // initialize a new array for use later
+    var txt = [];
     
-    // initially hide the second button (Dec 5.)
-    $("#dec5").hide();    
+    // add a first item to the array
+    txt[1] = "My notes for Nov. 12." ;   
     
-    //when the user clicks on Nov. 7 button, the modal is filled with the correct content
-    $("#nov12").on("click",function(){
-        fillNote(novTxt,this.id);
+    //when the user clicks, the modal is filled with the correct content
+   
+   // when clicking a note, fill with correct content 
+    $("#yournotes").on("click",".notes",function(){
+        theid = this.id;
+        fillNote();
     });
     
-    // when the user clicks on Dec. 5 button, the modal is filled with the correct content
-    $("#dec5").on("click",function(){
-        fillNote(decTxt,this.id);
-    });
-    
-    // when the user clicks "Add Note" it creates a new button
     $("#new").on("click",function(){
-        fillNote(decTxt,"dec5");
+        addNew();
     });
+    
+    // script to add new button
+    function addNew(){
+        i++; // increment +1
+        theid = i;
+        $("<div>",{class:"notes",id:i}).appendTo("#yournotes");
+        $("<button>",{type:"button",class:"btn btn-primary",id:"note"+i}).appendTo("#"+i);
+        $("#comment").val("");
+        $("#note"+i).attr( "data-toggle", "modal" ).attr( "data-target", "#note" ).text("Note - "+timeStamp());
+        console.log(i);
+    }
     
     // function that will handle placing the correct text inside the modal textbox
-    function fillNote(txt,id){
-        $("#comment").text(txt);
+    function fillNote(){
         
-        if(id == "nov12"){
-            $("#note h4").text("Notes - Nov. 12");
-            novTxt = txt; // update novTxt with latest updates
-            $("#comment").val(novTxt);
-            console.log("novTxt="+novTxt);
-        } else if (id == "dec5"){
-            $("#note h4").text("Notes - Dec. 5");
-            decTxt = txt; // update decTxt with latest updates
-            $("#comment").val(decTxt);
-            console.log("decTxt="+decTxt);
+        $("#comment").val(txt[theid]);
+        
+        for(var index in txt){
+            console.log(index+" "+txt[index]);
         }
+            
     }
     
     // when user closes the modal, save any text edits
@@ -46,16 +52,43 @@ $(document).ready(function(){
     
     // function to handle the close+save process
     function closeModal(){
+        console.log("theid: "+theid+", i:"+i);
+        txt[theid] = $("#comment").val();
         
-        if($("#note h4").text() == "Notes - Nov. 12"){
-            novTxt = $("#comment").val();
-        }
-        else if($("#note h4").text() == "Notes - Dec. 5"){
-            $("#dec5").show();
-            decTxt = $("#comment").val();
-        }
     }
     
-    
+    /*
+    Return a timestamp with the format "m/d/yy h:MM:ss TT"
+    */
+
+    function timeStamp() {
+    // Create a date object with the current time
+      var now = new Date();
+
+    // Create an array with the current month, day and time
+      var date = [ now.getMonth() + 1, now.getDate() ];
+
+    // Create an array with the current hour, minute and second
+      var time = [ now.getHours(), now.getMinutes() ];
+
+    // Determine AM or PM suffix based on the hour
+      var suffix = ( time[0] < 12 ) ? "AM" : "PM";
+
+    // Convert hour from military time
+      time[0] = ( time[0] < 12 ) ? time[0] : time[0] - 12;
+
+    // If hour is 0, set it to 12
+      time[0] = time[0] || 12;
+
+    // If seconds and minutes are less than 10, add a zero
+      for ( var i = 1; i < 3; i++ ) {
+        if ( time[i] < 10 ) {
+          time[i] = "0" + time[i];
+        }
+      }
+
+    // Return the formatted string
+      return date.join("/") + " " + time.join(":") + " " + suffix;
+    }
     
 });
